46b12298e50c15d27a49e9c2ccaa03e0a4488ed6
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-28 23:32:04 +0200

    wrote the auth page

--- src/pages/Auth/AuthPage.jsx
+++ src/pages/Auth/AuthPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Auth, AuthItem, BtsWrapper, Title, TitleSpan, ImgWrapper, Img } from './AuthPage.styled';
+import { Button } from '../../components/Button/Button';
+import Icons from '../../components/Icons/Icons';
+import imgBg from '../../img/auth/auth1.png';
 
 export const AuthPage = () => {
     return (
-        <div>Auth</div>
+        <Auth>
+            <AuthItem>
+                <Title>
+                    Transforming your <TitleSpan>body<Icons icon={'circle'}/></TitleSpan> shape with Power Pulse
+                </Title>
+                <BtsWrapper>
+                    <Button text={'Sign up'} background={'#E6533C'}/>
+                    <Button text={'Sign in'} background={'transparent'}/>
+                </BtsWrapper>
+            </AuthItem>
+            <ImgWrapper>
+                <Img src={imgBg} alt="" />
+            </ImgWrapper>
+        </Auth>
     );
 }
